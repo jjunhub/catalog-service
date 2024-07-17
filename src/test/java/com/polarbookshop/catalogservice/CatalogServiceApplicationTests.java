91c60f8e430620cc2257390f4b440c38681e5830
sangjun <sangjun1389@ajou.ac.kr>
2024-07-17 21:39:55 +0900

    CHAPTER 3 구현

--- src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -1,13 +1,36 @@
 package com.polarbookshop.catalogservice;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+import com.polarbookshop.catalogservice.domain.Book;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(
+        webEnvironment = WebEnvironment.RANDOM_PORT
+)
 class CatalogServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private WebTestClient webTestClient;
 
+    @Test
+    void whenPostRequestThenBookCreated() {
+        var expectedBook = new Book("1231231231", "Title", "Author", 9.90);
+
+        webTestClient
+                .post() // HTTP Method 지정
+                .uri("/books") // 엔드포인트 지정
+                .bodyValue(expectedBook) // Request Body에 담길 내용 지정
+                .exchange() // 요청을 전송한다.
+                .expectStatus().isCreated() // CREATED 200 상태코드가 오는지 확인
+                .expectBody(Book.class).value(actualBook -> {
+                    assertThat(actualBook).isNotNull();
+                    assertThat(actualBook.isbn()).isEqualTo(expectedBook.isbn());
+                });
+    }
 }
---
+++ src/test/java/com/polarbookshop/catalogservice/web/BookControllerTests.java
@@ -0,0 +1,35 @@
+package com.polarbookshop.catalogservice.web;
+
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.polarbookshop.catalogservice.domain.BookNotFoundException;
+import com.polarbookshop.catalogservice.domain.BookService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(BookController.class)
+public class BookControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BookService bookService;
+
+    @Test
+    void whenGetBookNotExistingThenShouldReturn404() throws Exception {
+        String isbn = "73737313940";
+        given(bookService.viewBookDetails(isbn))
+                .willThrow(BookNotFoundException.class); // 모의 빈이 어떻게 동작할 것인지 규정한다.
+
+        mockMvc
+                .perform(get("/books/" + isbn))
+                .andExpect(status().isNotFound());
+    }
+}
